fce0f5aa03782e5f24ebb728884b2e5109fe005c
Morte Mors <o-morte@hotmail.com>
2020-09-11 14:57:09 -0300

    change route

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ import Coronavirus from './app/models/Coronavirus'
 const routes = new Router();
 
 
+routes.get('/', (req,res) =>{
+  res.send({message: 'ok'})
+})
 routes.get('/update', CountrysController.store)
 routes.get('/search', CountrysSearch.store)
 
