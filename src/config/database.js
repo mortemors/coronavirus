10f1afe00aee51d314d5322954f7f203c4734c63
Morte Mors <o-morte@hotmail.com>
2020-09-11 12:38:54 -0300

    fix configure database

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'postgres://zoerrtrxofhgrh:bbcf490b52fd609eb69675d6bdc4abbdce12459710340afb0ff8b6aaeb6c3626@ec2-34-202-88-122.compute-1.amazonaws.com:5432/dcu55kj3phtr7p',
-  username: 'postgres',
-  password: 'docker',
-  database: 'pgdb',
+  host: "ec2-34-202-88-122.compute-1.amazonaws.com",
+  username: 'zoerrtrxofhgrh',
+  password: 'bbcf490b52fd609eb69675d6bdc4abbdce12459710340afb0ff8b6aaeb6c3626',
+  database: 'dcu55kj3phtr7p',
   define: {
     timestamps: true,
     underscored: true,
