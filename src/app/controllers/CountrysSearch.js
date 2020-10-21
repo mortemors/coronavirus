d4e9aa9cc830af7600124122a69e3bc64f320110
Morte Mors <jefersongb@outlook.com.br>
2020-10-21 18:50:23 -0300

    refactor: add statusCode to CountrySearch responses

--- src/app/controllers/CountrysSearch.js
+++ src/app/controllers/CountrysSearch.js
@@ -4,14 +4,18 @@ import fetch from 'node-fetch';
 class CountrySearch {
 
   async find(req, res) {
-    const result = await Coronavirus.findAll(
-      {
-        order:[
-          ['id', 'ASC']
-        ]
-      }
-    );
-      res.send(result)
+    try {
+      const result = await Coronavirus.findAll(
+        {
+          order:[
+            ['id', 'ASC']
+          ]
+        }
+      );
+      res.status(200).send(result)
+    } catch (error) {
+      res.status(500).send('There has been a problem with your operation: ' + error.message);
+    }
   }
 }
 export default new CountrySearch();
